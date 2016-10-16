8106d2397fe444bb25018151cd365d16a9b39021
MurphysL <1619673065@qq.com>
2016-10-16 15:16:51 +0800

    一个简易贴吧图片爬虫

---
+++ crawler.py
@@ -0,0 +1,40 @@
+import re
+import urllib.request 
+import sys
+
+print(sys.getdefaultencoding())
+
+def getHtml(url):
+    html = urllib.request.urlopen(url).read()
+    html = html.decode('utf-8')
+    return html
+
+def getImg(html):
+    reg = r' src="(.*?\.jpg)" pic_ext="jpeg"'
+    img = re.compile(reg)
+    imagelist = img.findall(html)
+    
+    x=0
+    print("start")
+    for imgurl in imagelist:
+        print(imgurl)
+        urllib.request.urlretrieve(imgurl, "%s.jpg" % x)
+        x += 1
+    print("finish")
+
+print(getHtml("http://tieba.baidu.com/p/3885457501"))
+getImg(getHtml("http://tieba.baidu.com/p/3885457501"))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
